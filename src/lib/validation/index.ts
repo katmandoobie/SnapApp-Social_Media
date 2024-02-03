f235a5852b48eac1ac15bea62659be5987222830
Alex <cater.sterling@gmail.com>
2024-02-02 23:27:31 -0800

    first commit

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,10 @@
+import * as z from "zod"
+
+
+export const SignupValidation = z.object({
+    name: z.string().min(2, {message: "Too short"}),
+    username: z.string().min(2, {message: "Too short"}).max(50),
+    email: z.string().email(),
+    password: z.string().min(8, {message: 'Must be at least 8 characters'}),
+  })
+  
